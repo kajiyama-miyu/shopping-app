bf2b5818d4c94c334cb10583b88a773abc493099
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-03-31 09:30:12 +0900

    loadingのアクション

--- src/reducks/loading/actions.ts
+++ src/reducks/loading/actions.ts
@@ -2,6 +2,7 @@ export const SHOW_LOADING = "SHOW_LOADING";
 export const HIDE_LOADING = "HIDE_LOADING";
 
 export const showLoadingAction = (text = "loading...") => {
+  console.log("text", text);
   return {
     type: SHOW_LOADING,
     payload: {
