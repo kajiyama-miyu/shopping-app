91c05885180aab4ce2b52d0fe8b9495be3fead23
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-03-31 09:32:26 +0900

    userの非同期処理

--- src/reducks/user/operations.ts
+++ src/reducks/user/operations.ts
@@ -1,17 +1,17 @@
 import axios from "axios";
 import { showLoadingAction, hideLoadingAction } from "../loading/actions";
 import { push } from "connected-react-router";
-import { User, Login } from "./type";
-import { signInAction } from "./actions";
+import { User, Login, AddCart, Order } from "./type";
+import { fetchCartAction } from "./actions";
 
 export const signUp = (user: User) => {
   return async (dispatch: any) => {
     const {
-      userName,
+      name,
       email,
       password,
       confirmPassword,
-      zipCode,
+      zipcode,
       address,
       telephone,
     } = user;
@@ -22,17 +22,17 @@ export const signUp = (user: User) => {
     }
 
     return axios
-      .post("url", {
-        userName: userName,
+      .post("http://35.73.116.71/api/register/", {
+        name: name,
         email: email,
         password: password,
-        zipCode: zipCode,
+        zipcode: zipcode,
         address: address,
         telephone: telephone,
       })
       .then(() => {
         dispatch(showLoadingAction("Sign up..."));
-        dispatch(push("/signin"));
+        dispatch(push("/"));
         dispatch(hideLoadingAction());
       })
       .catch((error) => {
@@ -46,28 +46,93 @@ export const signUp = (user: User) => {
 export const signIn = (loginUser: Login) => {
   return async (dispatch: any) => {
     const { email, password } = loginUser;
+    dispatch(showLoadingAction("Sign in..."));
 
-    return axios
-      .post<string>("url", {
+    return await axios
+      .post<string>("http://35.73.116.71/api/auth/login/", {
         email: email,
         password: password,
       })
       .then((res) => {
-        dispatch(showLoadingAction("Sign in..."));
+        const token = res.data;
+        const arr = Object.values(token);
+        const tokenSt = arr[0];
 
         if (!res.data) {
           dispatch(hideLoadingAction());
           alert("ユーザーが存在しません。ユーザー登録してください。");
           throw new Error("ユーザーが存在しません。");
         }
-
-        dispatch(signInAction({ isSignedIn: true, loginTokrn: res.data }));
+        //ローカルストレージにトークンを保存
+        localStorage.setItem("token", tokenSt);
 
         dispatch(hideLoadingAction());
-        dispatch(push("/itemlist"));
+        dispatch(push("/showList"));
       })
       .catch(() => {
+        alert("メールアドレスまたはパスワードが間違っています");
         dispatch(hideLoadingAction());
+      });
+  };
+};
+
+export const signOut = (authorization: string) => {
+  return async (dispatch: any) => {
+    return await axios
+      .post("http://35.73.116.71/api/auth/logout/", {
+        headers: {
+          Authorization: authorization,
+        },
+      })
+      .then(() => {
+        localStorage.clear();
+
+        dispatch(push("/"));
+      });
+  };
+};
+
+export const addProductsCart = (orderItem: AddCart) => {
+  return async (dispatch: any) => {
+    const {
+      order_item: { item, order_toppings, size, quantity },
+      status,
+    } = orderItem;
+
+    return axios
+      .post(
+        "http://35.73.116.71/api/add_cart/",
+        {
+          order_item: {
+            item: item,
+            size: size,
+            quantity: quantity,
+            order_toppings: order_toppings,
+          },
+          status: status,
+        },
+        { headers: { Authorization: localStorage.getItem("token") } }
+      )
+      .then(() => {
+        dispatch(push("/cart"));
+      })
+      .catch((error) => {
+        alert("カート追加に失敗しました。もう一度追加してください。");
+        throw new Error(error);
+      });
+  };
+};
+
+export const showCart = () => {
+  return async (dispatch: any) => {
+    return await axios
+      .get<Order>("http://35.73.116.71/api/add_cart/", {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        dispatch(fetchCartAction(res.data));
       });
   };
 };
