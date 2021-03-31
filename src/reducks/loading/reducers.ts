cc06b9478db7d51113bb6681961fcad90790e14e
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-03-31 09:30:28 +0900

    loadingのリデューサー

--- src/reducks/loading/reducers.ts
+++ src/reducks/loading/reducers.ts
@@ -1,16 +1,18 @@
 import * as Actions from "./actions";
 import { initialLoadingState } from "../store/initialState";
+import { Reducer } from "redux";
+import { Loading } from "./type";
 
-export const LoadingReducer = (
+export const LoadingReducer: Reducer<Loading, Actions.Actions> = (
   state = initialLoadingState,
-  action: Actions.Actions
+  action
 ) => {
   const { type, payload } = action;
   switch (type) {
     case Actions.SHOW_LOADING:
-      return { ...state, payload };
+      return { ...state, ...payload };
     case Actions.HIDE_LOADING:
-      return { ...state, payload };
+      return { ...state, ...payload };
     default:
       return state;
   }
