d3c5d49948748a6c8656d6710c004794117e5c88
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-04-08 10:52:42 +0900

    テスト

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Router } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { history, store } from "./reducks/store/rootReducer";
+import { Provider } from "react-redux";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+  );
+  const linkElement = screen.queryByText(/learn react/i);
+  expect(linkElement).toBeNull();
 });
